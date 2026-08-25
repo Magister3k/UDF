58a5ab1fbe7242734e4cf00bd0e3e136a9052314
Magister <magister2k@gmail.com>
2026-08-26 02:50:26 +0300

    build: переход на сборку через CMake и обновление параметров
    - Добавлен CMakeLists.txt для управления сборкой проекта
    - Удалены устаревшие make-файлы (makefile, makefile_1, makefile_2)
    - Удален вспомогательный скрипт check_deps.ps1 и скомпилированные файлы
    - В sosna_udf.cpp изменено соглашение о вызовах функции transcode_g723 с __stdcall на __cdecl
    - Добавлен список задач TODO.md и обновлен файл конфигурации .kilo/kilo.jsonc

--- check_deps.ps1
+++
@@ -1,8 +0,0 @@
-$dllPath = "D:\\Projects\\C++\\UDF\\release\\sosna_udf.dll"
-$dependsPath = "D:\\Programs\\MinGW\\depends32\\depends.exe"
-
-if (Test-Path $dependsPath) {
-    & $dependsPath $dllPath
-} else {
-    Write-Host "Dependency Walker не найден по пути $dependsPath"
-}
